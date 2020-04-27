acbc56e0f03b2d65ac4370f65465da84deb67b41
권주희 <akrso06197@khu.ac.kr>
2020-04-27 20:52:52 +0900

    trying to fix error

--- app/src/main/java/com/example/cctedv/RecordActivity.java
+++ app/src/main/java/com/example/cctedv/RecordActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.SurfaceHolder;
 import android.view.Window;
@@ -26,35 +27,31 @@ public class RecordActivity extends Activity implements RtspClient.Callback,
     // surfaceview
     private static net.majorkernelpanic.streaming.gl.SurfaceView mSurfaceView;
 
-    // Rtsp session
+    // RTSP session
     private Session mSession;
     private static RtspClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        // getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
 //        setContentView(R.layout.activity_record);
 
-        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
         mSurfaceView.getHolder().addCallback(this);
+
         setContentView(R.layout.activity_record);
 
         // Initialize RTSP client
         initRtspClient();
-
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         toggleStreaming();
     }
 
@@ -96,6 +93,37 @@ public class RecordActivity extends Activity implements RtspClient.Callback,
         mClient.setServerAddress(ip, Integer.parseInt(port));
         mClient.setStreamPath("/" + path);
     }
+
+//    private void initRtspClient() {
+//        // Configures the SessionBuilder
+//        mSession = SessionBuilder.getInstance()
+//                .setContext(getApplicationContext())
+//                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+//                .setAudioQuality(new AudioQuality(8000, 16000))
+//                .setVideoEncoder(SessionBuilder.VIDEO_H264)
+//                .setSurfaceView(mSurfaceView).setPreviewOrientation(0)
+//                .setCallback(this).build();
+//
+//        // Configures the RTSP client
+//        mClient = new RtspClient();
+//        mClient.setSession(mSession);
+//        mClient.setCallback(this);
+//        mSurfaceView.setAspectRatioMode(SurfaceView.ASPECT_RATIO_PREVIEW);
+//        String ip, port, path;
+//
+//        // We parse the URI written in the Editext
+//        Pattern uri = Pattern.compile("rtsp://(.+):(\\d+)/(.+)");
+//        Matcher m = uri.matcher(AppConfig.STREAM_URL);
+//        m.find();
+//        ip = m.group(1);
+//        port = m.group(2);
+//        path = m.group(3);
+//
+//        mClient.setCredentials(AppConfig.PUBLISHER_USERNAME,
+//                AppConfig.PUBLISHER_PASSWORD);
+//        mClient.setServerAddress(ip, Integer.parseInt(port));
+//        mClient.setStreamPath("/" + path);
+//    }
 
     private void toggleStreaming() {
         if (!mClient.isStreaming()) {
