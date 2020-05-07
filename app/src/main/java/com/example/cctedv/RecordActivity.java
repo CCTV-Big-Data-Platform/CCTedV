e37958d8c861abe7b3e4cdf9a9e81b850965d345
권주희 <akrso06197@khu.ac.kr>
2020-05-07 18:53:25 +0900

    make file from the camer buffer

--- app/src/main/java/com/example/cctedv/RecordActivity.java
+++ app/src/main/java/com/example/cctedv/RecordActivity.java
@@ -4,17 +4,38 @@ import android.app.Activity;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.TextureView;
-import android.widget.Button;
 import android.widget.FrameLayout;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class RecordActivity extends Activity implements TextureView.SurfaceTextureListener {
     private Camera mCamera;
     private TextureView mTextureView;
+
+    private String mOutputFile; // 파일
+    private DateFormat mDateFormat;
+    private String mDate;
+    private String mUserName = "victoria";
+    public File directory;
+    private FileOutputStream mFileOutputStream;
+    private File mFiles;
+    private boolean isCameraOpen = false;
+
+    private int mUnitTime = 1000;
+    private int mRemainingFileSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +46,45 @@ public class RecordActivity extends Activity implements TextureView.SurfaceTextu
         mTextureView.setSurfaceTextureListener(this);
 
         setContentView(mTextureView);
-
     }
 
+    public void settingVideoInfo() {
+        TimeZone mTimeZone = TimeZone.getDefault();
+        mDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        mDateFormat.setTimeZone(mTimeZone);
+        mDate = mDateFormat.format(new Date());
+        mOutputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/CCTedV" + "/" + mUserName + "_" + mDate;
+        Log.i("PATH", mOutputFile);
+        mFiles = new File(mOutputFile);
+
+        mRemainingFileSize = calculateGap(mDate)*44100*2;
+        try {
+            mFileOutputStream = new FileOutputStream(mOutputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makeDir(){
+        directory = new File(Environment.getExternalStorageDirectory() + File.separator + "CCTedV");
+        boolean success = true;
+        if (!directory.exists()) {
+            success = directory.mkdirs();
+        }
+        if (success) {
+            Log.v("FILE", "Directory is exist");
+        } else {
+            Log.e("FILE", "Directory not created");
+        }
+    }
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-        mCamera = Camera.open();
+        makeDir();
+        settingVideoInfo();
 
+        isCameraOpen = true;
+        mCamera = Camera.open();
 
         Camera.Size previewSize = mCamera.getParameters().getPreviewSize();
         mTextureView.setLayoutParams(new FrameLayout.LayoutParams(
@@ -47,7 +99,21 @@ public class RecordActivity extends Activity implements TextureView.SurfaceTextu
         mCamera.setPreviewCallback(new Camera.PreviewCallback() {
 
             public void onPreviewFrame(final byte[] data, final Camera camera) {
-                Log.i("WANT : ", data.toString());
+                if(isCameraOpen) {
+                    if(!accumulateFile(data)) {
+                        if (mFiles.exists()) {
+                            try {
+                                mFileOutputStream.close();
+                                mFileOutputStream = null;
+                                Log.i("MAKE : ", "file");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            settingVideoInfo();
+                        }
+                    }
+                }
                 // Process the contents of byte for whatever you need
             }
         });
@@ -60,13 +126,52 @@ public class RecordActivity extends Activity implements TextureView.SurfaceTextu
 
     @Override
     public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        isCameraOpen = false;
+        try {
+            mFileOutputStream.close();
+            mFileOutputStream = null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         mCamera.stopPreview();
         mCamera.release();
         return true;
+    }
+
+
+    private boolean accumulateFile(byte[] byteBuffer) {
+        try {
+            if(mRemainingFileSize >0) {
+                if(byteBuffer != null) {
+                    mFileOutputStream.write(byteBuffer);
+                    mRemainingFileSize -= byteBuffer.length * 2;
+                    return true;
+                } else {
+//                    Toast.makeText(RecorderService.this, "Playing...", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                mFileOutputStream.close();
+            }
+        }catch (IOException e){
+            Log.e("file out" , e.toString());
+        }
+        return false;
+    }
+
+    private int calculateGap(String date) {
+        String time = date.substring(11, date.length());
+        final int hour = Integer.parseInt(time.substring(0, 2));
+        final int min = Integer.parseInt(time.substring(3, 5));
+        final int sec = Integer.parseInt(time.substring(6, time.length()));
+        final int timeInSecond = hour * 3600 + min * 60 + sec;
+        final int gap = mUnitTime - (timeInSecond % mUnitTime);
+        return gap;
     }
 
     @Override
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
 
     }
+
 }
